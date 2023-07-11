b4ce41c03f5980c9ffca554dde9b5c5a5f60a379
yash <52443096+yashgupta-g@users.noreply.github.com>
2023-07-11 15:41:31 +0530

    updated

--- src/Loginpage.js
+++ src/Loginpage.js
@@ -8,15 +8,15 @@ function Loginpage() {
     const responseGoogle = (response) => {
         console.log(response);
     };
-    
+
     return (
 
 
-       
+
         <div className="main flex w-full bg-gray-100 flex-wrap ">
 
             {/* left container  */}
-            
+
             <div className="left-side hidden object-cover sm:block h-full">
                 <div className=" items-center flex justify-center h-screen">
                     <h1 className=" font-montserrat text-7xl text-white w-61 h-20 ">
@@ -26,39 +26,33 @@ function Loginpage() {
             </div>
 
             {/* right container  */}
-            <div className='right-side  '>
-
-                
-<div >
-              <form>
-                   
+            <div className='right-side'>
+                <div >
+                    <form>
                         <div className='  Container w-full  flex flex-col text-left mb-10   '>
                             <h2 className=' text-6xl font-montserrat font-bold mb-2'>Sign In</h2>
                             <h3 className=' heading '>Sign in to your account</h3>
                         </div>
-                    
+                        {/* auth buttons */}
+                        <div className='Container w-full flex flex-row  items-center mb-10  '>
+                            <GoogleLogin
+                                clientId="YOUR_GOOGLE_CLIENT_ID"
+                                buttonText="Sign in with Google "
+                                onSuccess={responseGoogle}
+                                onFailure={responseGoogle}
+                                cookiePolicy={'single_host_origin'}
+                                className='auth '
+                            />
 
-                            {/* auth buttons */}
-                            <div className='Container w-full flex flex-row  items-center mb-10  '>
+                            <button className=' apple-button'>
+                                <img src="https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-dallas-shootings-don-add-are-speech-zones-used-4.png" alt="apple" className=" h-3 w-3.5 inline-block m-4  " />
+                                <p className=' text-gray-400'>Sign in with Apple </p>
+                            </button>
+                        </div>
+                    </form>
 
-                                <GoogleLogin
-                                    clientId="YOUR_GOOGLE_CLIENT_ID"
-                                    buttonText="Sign in with Google "
-                                    onSuccess={responseGoogle}
-                                    onFailure={responseGoogle}
-                                    cookiePolicy={'single_host_origin'}
-                                    className='auth '
-                                />
-
-                                <button className=' apple-button'>
-                                    <img src="https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-dallas-shootings-don-add-are-speech-zones-used-4.png" alt="apple" className=" h-3 w-3.5 inline-block m-4  " />
-                                    <p className=' text-gray-400'>Sign in with Apple </p>
-                                </button>
-                            </div>
-                 </form>
-
-                {/*  form card */}
-                <form className=" formcontainer justify-center flex bg-white rounded-2xl  ">
+                    {/*  form card */}
+                    <form className=" formcontainer justify-center flex bg-white rounded-2xl  ">
                         <div className=" form-content  ">
                             {/* email */}
                             <label
@@ -98,19 +92,16 @@ function Loginpage() {
                                     </button>
                                 </div>
                             </div>
-
-                        </div> 
-                </form>
-               
-                    
+                        </div>
+                    </form>
                     {/* card footer */}
                     <div className='w-full flex items-center justify-center mt-5 '>
                         <p className='text-gray-600 heading '>Dont have account?<span className='heading cursor-pointer text-blue-600 '>Register here</span> </p>
                     </div>
-                    </div> 
                 </div>
             </div>
-       
+        </div>
+
     )
 }
 
